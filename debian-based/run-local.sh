fd3b864d01cd8b4100d82a9dcadd66bb5cd089b9
kdirectorate <kangell@alertra.com>
2024-02-13 17:57:06 -0600

    Changed name of playbook.

--- debian-based/run-local.sh
+++ debian-based/run-local.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook --ask-become-pass --connection=local --extra-vars "ansible_user=${USER}" baremetal-pb.yml
+ansible-playbook --ask-become-pass --connection=local --extra-vars "ansible_user=${USER}" basicdev-pb.yml
