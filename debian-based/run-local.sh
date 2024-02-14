546d84fdcb618d52b8b5e164e4eac86dc2f3a648
kdirectorate <kangell@alertra.com>
2024-02-14 10:46:22 -0600

    Moved the playbooks to their own directory.

--- debian-based/run-local.sh
+++ debian-based/run-local.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook --ask-become-pass --connection=local --extra-vars "ansible_user=${USER}" basicdev-pb.yml
+ansible-playbook --ask-become-pass --connection=local --extra-vars "ansible_user=${USER}" playbooks/basicdev.yml
